f82e1e80f783221ff90b9d2893b4ff41fcc83078
sunwei2 <sunwei2@9fbank.com.cn>
2019-09-11 17:32:42 +0800

    测试提交

---
+++ yaml/src/main/java/com/sw/yaml/YamlApplication.java
@@ -0,0 +1,22 @@
+package com.sw.yaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * com.sw.yaml
+ * <p>
+ * TODO
+ *
+ * @author 孙威
+ * @date 2019/6/29 19:10
+ */
+@SpringBootApplication
+public class YamlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YamlApplication.class, args);
+    }
+
+
+}
---
+++ yaml/src/main/java/com/sw/yaml/config/YamlConfig.java
@@ -0,0 +1,12 @@
+package com.sw.yaml.config;
+
+/**
+ * com.sw.yaml.jobconfig
+ * <p>
+ * TODO
+ *
+ * @author 孙威
+ * @date 2019/6/29 19:15
+ */
+public class YamlConfig {
+}
